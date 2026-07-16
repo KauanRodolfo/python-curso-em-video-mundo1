78f44fb3b7fa8ea6a938143a1b73882deab8a071
KauanRodolfo <rodolfokauan5@gmail.com>
2026-07-16 17:35:59 -0300

    Exercicios 003 e 004 prontos. aproveitei e comentei os códigos

---
+++ ex004.py
@@ -0,0 +1,10 @@
+#Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+n = input("Digite algo: ")
+print(f"O tipo primitivo desse valor é {type(n)}")
+print(f"Só tem espaços? {n.isspace()}")
+print(f"É um número? {n.isnumeric()}")
+print(f"É alfabético? {n.isalpha()}")
+print(f"É alfanúmerico? {n.isalnum()}")
+print(f"Está em maiúsculas? {n.isupper()}")
+print(f"Está em minúsculas? {n.islower()}")
